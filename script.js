945d91a06add2137ce3482fd40158a3ac15b14ba
Anuragkolhe <128228032+Anuragkolhe@users.noreply.github.com>
2023-05-24 13:16:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// //your JS code here. If required.
-// function removeOption(){
-//             let opt=document.getElementById("removeButton").addEventListener("click",removeOption);
-//             opt.remove(opt.selectedIndex);
-	        
-//         }
 
-//your JS code here. If required.
-// const removeBtn = document.getElementById("removeBtn");
-// removeBtn.addEventListener("click", function() {
-//   const select = document.getElementById("colorSelect");
-//   const index = select.selectedIndex; // Get the index of the selected option
-//   if (index !== -1) { // If an option is selected
-//     select.remove(index); // Remove the selected option from the list
-//   }
-// });
 
 
 const removeBtn = document.getElementById("removeButton");
